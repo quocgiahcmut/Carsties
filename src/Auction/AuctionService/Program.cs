163b95a069d7a98c0a95b6390b75882b1d1db690
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-11-24 16:58:03 +0700

    add dockerfile

--- src/Auction/AuctionService/Program.cs
+++ src/Auction/AuctionService/Program.cs
@@ -30,6 +30,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, config) =>
     {
+        config.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
         config.ConfigureEndpoints(context);
     });
 });
