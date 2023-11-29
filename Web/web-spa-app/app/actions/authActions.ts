43b0d9de0c4b29687bb2b4068efbdc46a755eb3a
Quoc Gia <67970731+quocgiahcmut@users.noreply.github.com>
2023-11-29 13:47:09 +0700

    finish simple identity for client app

--- Web/web-spa-app/app/actions/authActions.ts
+++ Web/web-spa-app/app/actions/authActions.ts
@@ -1,5 +1,8 @@
 import { getServerSession } from "next-auth";
+import { getToken } from "next-auth/jwt";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import { cookies, headers } from 'next/headers'
+import { NextApiRequest } from "next";
 
 export async function getSession() {
   return await getServerSession(authOptions)
@@ -17,4 +20,15 @@ export async function getCurrentUser() {
   } catch(err) {
     return null
   }
+}
+
+export async function getTokenWorkaround() {
+  const req = {
+    headers: Object.fromEntries(headers() as Headers),
+    cookies: Object.fromEntries(
+      cookies().getAll().map(c => [c.name, c.value])
+    )
+  } as NextApiRequest
+
+  return await getToken({req})
 }
--- Web/web-spa-app/middleware.ts
+++ Web/web-spa-app/middleware.ts
@@ -3,5 +3,8 @@ export { default } from 'next-auth/middleware'
 export const config = {
   matcher: [
     '/session'
-  ]
+  ],
+  pages: {
+    signIn: '/api/auth/signin'
+  }
 }
